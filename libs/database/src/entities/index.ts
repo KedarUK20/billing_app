7563561a90020d08555503371017e6565c978984
Kedar Kumbhar <kedark7861920@gmail.com>
2026-04-01 23:32:55 +0530

    added productentity

--- libs/database/src/entities/index.ts
+++ libs/database/src/entities/index.ts
@@ -1,8 +1,11 @@
 import { CustomerEntity } from './customer.entity';
+import { ProductEntity } from './product.entity';
 import { UserEntity } from './user.entity';
 
-export const entities = [UserEntity, CustomerEntity];
+export const entities = [UserEntity, CustomerEntity, ProductEntity];
 
 export * from './customer.entity';
 
 export * from './user.entity';
+
+export * from './product.entity';

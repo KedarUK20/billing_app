fb4c2dbab95745688e209d6794ccae12931b0d3f
Kedar Kumbhar <kedark7861920@gmail.com>
2026-04-01 23:27:25 +0530

    added customerentity

--- libs/database/src/entities/index.ts
+++ libs/database/src/entities/index.ts
@@ -1,5 +1,8 @@
+import { CustomerEntity } from './customer.entity';
 import { UserEntity } from './user.entity';
 
-export const entities = [UserEntity];
+export const entities = [UserEntity, CustomerEntity];
+
+export * from './customer.entity';
 
 export * from './user.entity';
--- libs/database/src/migrations/index.ts
+++ libs/database/src/migrations/index.ts
@@ -1,3 +1,4 @@
 import { Migration1775034832465 } from "./1775034832465-migration";
+import { Migration1775065573895 } from "./1775065573895-migration";
 
-export const migrations = [Migration1775034832465];
+export const migrations = [Migration1775034832465, Migration1775065573895];
